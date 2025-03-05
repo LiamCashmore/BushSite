81043281674843efb445c102b4b88ea92d57bd3f
Gamehenge <liamcash10@gmail.com>
2025-03-05 16:43:28 -0700

    popup added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// if ('scrollRestoration' in history) {
-//     history.scrollRestoration = 'manual';
-// }
-
-// window.onload = function() {
-//     window.scrollTo(0, 0);
-// };
+window.addEventListener('load', function() {
+    // Only show the popup if it hasn't been shown in this session
+    if (!sessionStorage.getItem('popupDisplayed')) {
+      document.getElementById('popup-overlay').style.display = 'flex';
+    }
+  });
+  
+  // Hide the popup when the close button is clicked and store the flag in sessionStorage
+  document.getElementById('close-popup').addEventListener('click', function() {
+    document.getElementById('popup-overlay').style.display = 'none';
+    sessionStorage.setItem('popupDisplayed', 'true');
+  });
